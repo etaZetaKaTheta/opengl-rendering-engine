91b7302633ebd69328bd2dd03f90cdb5fea07f38
Ratchgamer123 <trautwein.maximilian04@gmail.com>
2022-03-25 00:27:41 +0100

    window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
-#include "libraries/include/glad/glad.h"
+#include "glad/glad.h"
+#include "glfw/glfw3.h"
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
 
 int main()
 {
-	std::cout << "Hello, world!";
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "opengl-rendering-engine", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Window creation failed" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	glClearColor(0.6, 0.776, 0.556, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
 }
